64ef66541b2c79a9037c8d734214da54320c71cc
khrodriguezc <khrodriguezc@unal.edu.co>
2023-04-12 18:01:40 -0500

    Generación de api en uvicorn y fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/diccionario')
+def get_dict():
+    return {'name': 'Platzy'}
+
+@app.get('/html', response_class=HTMLResponse)
+def get_html():
+    return """
+        <h1>Título de la página web</h1>
+        <p>Primer parrafo de mi primera web</p>
+        """
 def run():
     store.get_categories()
 
